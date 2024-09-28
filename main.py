9bc0e4749d35a4b8c3e3df4bf7252186b9f63b37
Domagoj <paulic.domagoj@gmail.com>
2024-09-28 20:40:51 +0200

    impl chat chain 22

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import LLMChain
 from langchain.prompts import HumanMessagePromptTemplate, ChatPromptTemplate
+from dotenv import load_dotenv
+
+load_dotenv()
+
+chat = ChatOpenAI()
 
 prompt = ChatPromptTemplate(
     input_variables=["content"],
@@ -7,7 +14,14 @@ prompt = ChatPromptTemplate(
     ]
 )
 
+chain = LLMChain(
+    llm=chat,
+    prompt=prompt
+)
+
 while True:
     content = input(">> ")
 
-    print(f"You entered: {content}")
+    result = chain({"content": content})
+
+    print(result["text"])
