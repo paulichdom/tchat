203485fa77b741548402eba8e2eaeacd20e1707c
Domagoj <paulic.domagoj@gmail.com>
2025-08-23 22:28:31 +0200

    enhance chat prompt template to include messages variable and memory management

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 from langchain_openai import ChatOpenAI
 from langchain.chains import LLMChain
-from langchain.prompts import HumanMessagePromptTemplate, ChatPromptTemplate
+from langchain.prompts import MessagesPlaceholder, HumanMessagePromptTemplate, ChatPromptTemplate
+from langchain.memory import ConversationBufferMemory
 from dotenv import load_dotenv
 
 load_dotenv()
 
 chat = ChatOpenAI()
 
+memory = ConversationBufferMemory(memory_key="messages", return_messages=True)
+
 prompt = ChatPromptTemplate(
-    input_variables=["content"],
+    input_variables=["content", "messages"],
     messages=[
+        MessagesPlaceholder(variable_name="messages"),
         HumanMessagePromptTemplate.from_template("{content}")
     ]
 )
 
 chain = LLMChain(
     llm=chat,
-    prompt=prompt
+    prompt=prompt,
+    memory=memory,
 )
 
 while True:
