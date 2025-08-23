b9c57536879a1ed390349e1c83064c090d33cef6
Domagoj <paulic.domagoj@gmail.com>
2025-08-23 22:50:15 +0200

    refactor memory management to use ConversationSummaryMemory for improved message handling

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 from langchain_openai import ChatOpenAI
 from langchain.chains import LLMChain
 from langchain.prompts import MessagesPlaceholder, HumanMessagePromptTemplate, ChatPromptTemplate
-from langchain.memory import ConversationBufferMemory, FileChatMessageHistory
+from langchain.memory import ConversationSummaryMemory, FileChatMessageHistory
 from dotenv import load_dotenv
 
 load_dotenv()
 
 chat = ChatOpenAI()
 
-memory = ConversationBufferMemory(
-    chat_memory=FileChatMessageHistory("messages.json"),
+memory = ConversationSummaryMemory(
     memory_key="messages",
-    return_messages=True
+    return_messages=True,
+    llm=chat,
 )
 
 prompt = ChatPromptTemplate(
@@ -26,6 +26,7 @@ chain = LLMChain(
     llm=chat,
     prompt=prompt,
     memory=memory,
+    verbose=True
 )
 
 while True:
