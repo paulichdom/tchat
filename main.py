a165dfbccb7da3ef217ac5c05fc0d00d702bceef
Domagoj <paulic.domagoj@gmail.com>
2025-08-23 22:37:22 +0200

    save and extend conversations

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from langchain_openai import ChatOpenAI
 from langchain.chains import LLMChain
 from langchain.prompts import MessagesPlaceholder, HumanMessagePromptTemplate, ChatPromptTemplate
-from langchain.memory import ConversationBufferMemory
+from langchain.memory import ConversationBufferMemory, FileChatMessageHistory
 from dotenv import load_dotenv
 
 load_dotenv()
 
 chat = ChatOpenAI()
 
-memory = ConversationBufferMemory(memory_key="messages", return_messages=True)
+memory = ConversationBufferMemory(
+    chat_memory=FileChatMessageHistory("messages.json"),
+    memory_key="messages",
+    return_messages=True
+)
 
 prompt = ChatPromptTemplate(
     input_variables=["content", "messages"],
